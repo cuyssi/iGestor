2e4d9bb830b933dacdf584c57b3b1aa6c7e341a6
cuyssi <anabcastrolopez@gmail.com>
2026-06-18 11:18:38 +0200

    feat: create base frontend layout

--- front/src/app/app.routes.ts
+++ front/src/app/app.routes.ts
@@ -1,3 +1,37 @@
 import { Routes } from '@angular/router';
+import { AuthLayout } from './layout/auth-layout/auth-layout';
+import { MainLayout } from './layout/main-layout/main-layout';
+import { Home } from './pages/home/home';
+import { Login } from './pages/login/login';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full',
+  },
+  {
+    path: '',
+    component: AuthLayout,
+    children: [
+      {
+        path: 'login',
+        component: Login,
+      },
+    ],
+  },
+  {
+    path: '',
+    component: MainLayout,
+    children: [
+      {
+        path: 'home',
+        component: Home,
+      },
+    ],
+  },
+  {
+    path: '**',
+    redirectTo: 'login',
+  },
+];
