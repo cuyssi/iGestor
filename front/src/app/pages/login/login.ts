fabe53b4a3346980c5fd1ae18dc8b591eebafdd2
cuyssi <anabcastrolopez@gmail.com>
2026-06-29 12:16:13 +0200

    feat: implement login page layout and functionality with background images

--- front/src/app/pages/login/login.ts
+++ front/src/app/pages/login/login.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { LucideLock, LucideUser } from '@lucide/angular';
+import { Button } from '../../shared/button/Button';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  standalone: true,
+  imports: [LucideUser, LucideLock, Button],
   templateUrl: './login.html',
   styleUrl: './login.scss',
 })
 export class Login {
+  constructor(private router: Router) {}
+
+  login() {
+    this.router.navigate(['/home']);
+  }
 
 }
